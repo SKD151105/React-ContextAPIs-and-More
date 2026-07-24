1a692c3d1fb7fafdfe43a7636e0835350edb7567
SKD <skd1545passion@gmail.com>
2026-07-24 23:28:21 +0530

    feat: initialize mini-project-2 (state-efficient password generator) with React, Vite, and Tailwind CSS
    - Added package.json with dependencies and scripts for development
    - Created App.css for global styles
    - Implemented main App component with a title and Container component
    - Added Container component for layout
    - Included Tailwind CSS in index.css
    - Set up main entry point in main.jsx
    - Configured Vite with React and Tailwind CSS plugins

---
+++ Self-Learn/Mini_Project_2/src/App.jsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+import './App.css'
+import Container from './components/Container.jsx'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p className="text-2xl font-bold text-center my-4">Get your Unique Password!</p>
+      <Container />
+    </>
+  )
+}
+
+export default App
---
+++ Self-Learn/Mini_Project_2/src/components/Container.jsx
@@ -0,0 +1,6 @@
+export default function Container() {
+    return (
+        <div className="w-[90%] h-[200px] bg-white rounded-3xl text-violet-950 text-center font-bold text-2xl p-4 m-auto">
+        </div>
+    )
+}
